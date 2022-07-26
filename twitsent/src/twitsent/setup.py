de13a3fef212c450d8af0445ed84839f1fe67173
mhoikka <56174759+mhoikka@users.noreply.github.com>
2022-07-26 13:08:24 -0700

    added setup file

---
+++ twitsent/src/twitsent/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='twitsent',
+      version='0.0.7',
+      description='Twitter Sentiment Analysis package',
+      url='https://github.com/mhoikka/twitsent.git',
+      author='Mitchell Hoikka',
+      author_email='',
+      license='MIT',
+      packages=['twitsent'],
+      install_requires=[
+            'requests>=2.27.1',
+            'clean-text>=0.6.0',
+            'tkcalendar>=1.6.1',
+            'matplotlib>=3.5.2',
+            'pymannkendall>=1.4.2',
+            'nltk>=3.5',
+            'unidecode>=1.3.4'
+            ],
+      zip_safe=False)
