c794df306aa58c1b0c096ee0c2234192631a87a1
mhoikka <56174759+mhoikka@users.noreply.github.com>
2022-07-27 10:08:09 -0700

    formatted html page

--- twitsent/src/twitsent/makescript.py
+++ twitsent/src/twitsent/makescript.py
@@ -1,4 +1,3 @@
-# import module
 import codecs
 import webbrowser
 import os
@@ -38,26 +37,34 @@ def make_page():
     <head>
     <title>Tweet Sentiment Analysis Student Research</title>
     <basefont size=4>
+    <style>
+    .focus, h1, h2, h3{{
+        text-align: center;
+    }}
+    :not(hr, li){{
+        padding: 5px 15px;
+    }}
+    </style>
     </head>
 
     <body bgcolor=FFFFFF>
 
-    <h1 align=center>Twitter Sentiment Analysis With NLTK's Vader</h1>
-    <h2 align=center>(ACO 499) 2022 Summer Session B</h2>
+    <h1>Twitter Sentiment Analysis With NLTK's Vader</h1>
+    <h2>(ACO 499) 2022 Summer Session B</h2>
 
-    <h3 align=center>Mitchell Hoikka</h3>
+    <h3>Mitchell Hoikka</h3>
 
-    <p align=left>The purpose of this project is to detect and compare trends in sentiment data extracted from Tweets that contain certain keywords. Using the access tokens from a Twitter developer account, users can run historical
+    <p class="focus" align=left>The purpose of this project is to detect and compare trends in sentiment data extracted from Tweets that contain certain keywords. Using the access tokens from a Twitter developer account, users can run historical
     searches. Those searches will return Tweets that match certain specifications (such as containing keywords, and being written in a specific language), then plot the sentiment score data of those tweets against the scores of
     random Tweets in a matching time period. The sentiment score is determined using the NLTK Vader compound score value. The Tweet text is then stored in local files, and the
      data can be used again or appended by rerunning the program over a different time frame with matching user-defined parameters.</p>
 
-    <img src={mypath} alt="Graph comparing baseline sentiment to keyword sentiment scores">
+    <img class="focus" src={mypath} alt="Graph comparing baseline sentiment to keyword sentiment scores">
     <hr>
-    <p align=left>How to read the graph:<br>
-    As Y values(sentiment score) approach 1, the sentiment in tweets containing the keywords becomes more positive. There can be multiple causes of this, and it should <b>not</b> be assumed that 
+    <h3>How to read the graph:</h3>
+    <p>As Y values(sentiment score) approach 1, the average sentiment parsed from Tweets containing the specified keyword(s) becomes more positive. There can be multiple causes of this, and it should <b>not</b> be assumed that 
     increased positive sentiment related to a keyword implies positive sentiment <i>about</i> that keyword. For instance, if the keyword list is (covid, coronavirus) and the sentiment score increases over a period
-    of a week, people may be expressing that they feel positively about Covid-19- or perhaps that a lockdown was just lifted, or a vaccine was made. In other words, sentiment related to a specific set of keywords
+    of a week, people may be expressing that they feel more positively about Covid 19 recently- or perhaps that a lockdown was just lifted, or a vaccine was made available. In other words, sentiment related to a specific set of keywords
     may be incidental to those keywords rather than directly related to them. More specific keyword lists can reduce this effect, but it cannot be entirely removed.</p>
 
     <p align=left>A sentiment score value in the graph is calculated as the average of scores collected in a specified interval. Each of the initial scores are the compound sentiment score calculated by NLTK'S
@@ -66,8 +73,8 @@ def make_page():
     that in general, the sentiment score of Tweets containing the most common words correlates with the most common sentiments. </p>
 
     <hr>
-    <p align=left>Configuration Specific Details<br>
-    The Twitter Search API v2 allows users with elevated access to search 2 million Tweets each month, and users with academic access to search 10 million tweets each month. Users with elevated access
+    <h3>Configuration Specific Details</h3>
+    <p>The Twitter Search API v2 allows users with elevated access to search 2 million Tweets each month, and users with academic access to search 10 million tweets each month. Users with elevated access
     are limited to 450 requests per 15 minute interval using Recent Search(Up to one week in the past). Users with Academic access can use full archive search, which is limited to 300 requests per 15 minutes and 1 request per second.
     Each response to a request can contain up to 100 tweets.
     This translates to a maximum of roughly 2500 Tweets per hour when Tweet collection is consistent over a month, and up to 45,000 Tweets collected per hour for an elevated access user. 
